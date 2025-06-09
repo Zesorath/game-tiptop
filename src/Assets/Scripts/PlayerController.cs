8ffcde55a4f39011f13a108cb18655281d1d7989
hfg16 <hugogranadossoldevilla@my.unt.edu>
2025-06-08 19:24:37 -0500

    Add comment

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //Add Space function
         {
             Flap();
         }
