faa2d2bb7ebe917694608fa9130d6c0ba67a6246
Liam <lrsullivan98@gmail.com>
2025-06-29 21:14:31 -0500

    Update PlayerController.cs to add left movement, right movement, and gravity flip with Z, X, and Shift, respectively.

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float Force;
+    public float speed = 5f;
 
+    bool gravityFlip = false;
+    public float movement = 1f; //Sets movement to the right
     private Rigidbody2D rb;
     private GameController gc;
 
@@ -21,6 +24,25 @@ public class PlayerController : MonoBehaviour
         {
             Flap();
         }
+
+        if (Input.GetKeyDown(KeyCode.Z)) //Makes the player move left if they press "Z"
+        {
+            movement = -1f;
+        }
+        else if (Input.GetKeyDown(KeyCode.X)) //Makes the player move right if they press "X"
+        {
+            movement = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) //Flip gravity by pressing the "Shift" key
+        {
+            Flip();
+        }
+    }
+
+    void FixedUpdate() //
+    {
+        rb.velocity = new Vector2(movement * speed, rb.velocity.y);
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -34,5 +56,11 @@ public class PlayerController : MonoBehaviour
     public void Flap()
     {
         rb.AddForce(Vector2.up * Force);
+    }
+
+    public void Flip()
+    {
+        gravityFlip = !gravityFlip;
+        rb.gravityScale = gravityFlip ? -1f : 1f;
     }
 }
